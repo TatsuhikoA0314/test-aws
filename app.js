4ca7cbb4b59609d78ef3687383e3aa441bd34adc
荒川達彦 <57385542+TatsuhikoA0314@users.noreply.github.com>
2021-02-12 13:56:35 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+
+
 app.get('/', (req, res) => {
   res.send("hello");
 });
 
-
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
   console.log("success");
 });
